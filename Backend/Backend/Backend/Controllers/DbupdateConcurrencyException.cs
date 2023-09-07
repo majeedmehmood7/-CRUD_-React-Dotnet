14f0f5173eeccf5d8efe1a1b19db4afae1af94e0
majeedmehmood7 <majeedmehmood7@gmail.com>
2023-09-06 23:12:26 -0700

    Upload

---
+++ Backend/Backend/Backend/Controllers/DbupdateConcurrencyException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace Backend.Controllers
+{
+    [Serializable]
+    internal class DbupdateConcurrencyException : Exception
+    {
+        public DbupdateConcurrencyException()
+        {
+        }
+
+        public DbupdateConcurrencyException(string? message) : base(message)
+        {
+        }
+
+        public DbupdateConcurrencyException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected DbupdateConcurrencyException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Backend/Backend/Backend/Controllers/EmployeeController.cs
@@ -0,0 +1,119 @@
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeContext _employeeContext;
+
+        public EmployeeController(EmployeeContext employeeContext)
+        {
+            _employeeContext = employeeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            try
+            {
+                var employees = await _employeeContext.Employees.ToListAsync();
+                return Ok(employees);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            try
+            {
+                var employee = await _employeeContext.Employees.FindAsync(id);
+
+                if (employee == null)
+                {
+                    return NotFound("Employee not found");
+                }
+
+                return Ok(employee);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            try
+            {
+                _employeeContext.Employees.Add(employee);
+                await _employeeContext.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(GetEmployee), new { id = employee.ID }, employee);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutEmployee(int id, Employee employee)
+        {
+            if (id != employee.ID)
+            {
+                return BadRequest("Invalid ID");
+            }
+
+            try
+            {
+                _employeeContext.Entry(employee).State = EntityState.Modified;
+                await _employeeContext.SaveChangesAsync();
+                return Ok();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return NotFound("Employee not found");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteEmployee(int id)
+        {
+            try
+            {
+                var employee = await _employeeContext.Employees.FindAsync(id);
+
+                if (employee == null)
+                {
+                    return NotFound("Employee not found");
+                }
+
+                _employeeContext.Employees.Remove(employee);
+                await _employeeContext.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
